ad0959fe8e32c3aac4af3b6c8960c9843a1942f0
ubong-s <67463432+ubong-s@users.noreply.github.com>
2020-08-06 08:29:08 +0100

    Add files via upload

---
+++ Day-312-Countdown-Design/app.js
@@ -0,0 +1,92 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const countdownDate = document.querySelector('.countdown-date');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+// let futureDate = new Date(2020, 11, 31, 11, 30, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 50, 0, 0, 0);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+
+let weekday = weekdays[futureDate.getDay()];
+
+countdownDate.textContent = `${weekday}, ${date} ${month} ${year}.`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60mins
+  // 1day = 24hrs
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  // calculate all values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set values array
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class='expired'>Sorry, this promo has expired</h4>`;
+  }
+}
+
+// countdown
+let countdown = setInterval(getRemainingTime, 1000);
+getRemainingTime();
